2cd1bc6bfd79cc126c5460b9e7382f568e5450d7
huykaito454 <tronghuy.832001@gmail.com>
2022-07-12 15:19:49 +0700

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["Be Vietnam Pro", "sans-serif"],
+      },
+      colors: {
+        primary: "#00ab84",
+      },
+    },
+  },
+  plugins: [],
+};
